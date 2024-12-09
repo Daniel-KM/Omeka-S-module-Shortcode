694cc30d15f13eb3dfeb6c2ee639b66faef8ea83
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-12-09 00:00:00 +0000

    Replaced dependency from module Generic to module Common.

--- Module.php
+++ Module.php
@@ -5,7 +5,7 @@
  *
  * Insert shortcuts in site pages in order to render more content via a simple string.
  *
- * @copyright Daniel Berthereau, 2021
+ * @copyright Daniel Berthereau, 2021-2024
  * @license http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.txt
  *
  * This software is governed by the CeCILL license under French law and abiding
@@ -33,23 +33,33 @@
  */
 namespace Shortcode;
 
-if (!class_exists(\Generic\AbstractModule::class)) {
-    require file_exists(dirname(__DIR__) . '/Generic/AbstractModule.php')
-        ? dirname(__DIR__) . '/Generic/AbstractModule.php'
-        : __DIR__ . '/src/Generic/AbstractModule.php';
+if (!class_exists(\Common\TraitModule::class)) {
+    require_once dirname(__DIR__) . '/Common/TraitModule.php';
 }
 
-use Generic\AbstractModule;
+use Common\TraitModule;
 use Laminas\EventManager\Event;
 use Laminas\EventManager\SharedEventManagerInterface;
+use Omeka\Module\AbstractModule;
 
 class Module extends AbstractModule
 {
+    use TraitModule;
+
     const NAMESPACE = __NAMESPACE__;
 
     protected function preInstall(): void
     {
-        require_once __DIR__ . '/data/scripts/upgrade_vocabulary.php';
+        $services = $this->getServiceLocator();
+        $translate = $services->get('ControllerPluginManager')->get('translate');
+
+        if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.63')) {
+            $message = new \Omeka\Stdlib\Message(
+                $translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
+                'Common', '3.4.63'
+            );
+            throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
+        }
     }
 
     public function attachListeners(SharedEventManagerInterface $sharedEventManager): void
--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -2,7 +2,7 @@
 
 namespace Shortcode;
 
-use Omeka\Stdlib\Message;
+use Common\Stdlib\PsrMessage;
 
 /**
  * @var Module $this
@@ -11,17 +11,31 @@ use Omeka\Stdlib\Message;
  * @var string $oldVersion
  *
  * @var \Omeka\Api\Manager $api
+ * @var \Laminas\Log\Logger $logger
  * @var \Omeka\Settings\Settings $settings
  * @var \Doctrine\DBAL\Connection $connection
+ * @var \Omeka\Settings\SiteSettings $siteSettings
  * @var \Doctrine\ORM\EntityManager $entityManager
  * @var \Omeka\Mvc\Controller\Plugin\Messenger $messenger
  */
 $plugins = $services->get('ControllerPluginManager');
 $api = $plugins->get('api');
+$logger = $services->get('Omeka\Logger');
 $settings = $services->get('Omeka\Settings');
+$translate = $plugins->get('translate');
+// $translator = $services->get('MvcTranslator');
 $connection = $services->get('Omeka\Connection');
 $messenger = $plugins->get('messenger');
+$siteSettings = $services->get('Omeka\Settings\Site');
 $entityManager = $services->get('Omeka\EntityManager');
+
+if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.63')) {
+    $message = new \Omeka\Stdlib\Message(
+        $translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
+        'Common', '3.4.63'
+    );
+    throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
+}
 
 if (version_compare($oldVersion, '3.3.1.2', '<')) {
     require_once __DIR__ . '/upgrade_vocabulary.php';
@@ -32,7 +46,7 @@ if (version_compare($oldVersion, '3.3.1.6', '<')) {
 }
 
 if (version_compare($oldVersion, '3.3.1.7', '<')) {
-    $message = new Message(
+    $message = new PsrMessage(
         'It’s now possible to display the primary image of a resource with shortcut [image].' // @translate
     );
     $messenger->addSuccess($message);
