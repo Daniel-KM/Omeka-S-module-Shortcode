709fd6f29e35870fe9fc0ef7e9ac0259534255e4
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-07-13 00:00:00 +0000

    Updated languages.

--- config/module.config.php
+++ config/module.config.php
@@ -18,6 +18,16 @@ return [
             'shortcodes' => Service\ViewHelper\ShortcodesFactory::class,
         ],
     ],
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type' => \Laminas\I18n\Translator\Loader\Gettext::class,
+                'base_dir' => dirname(__DIR__) . '/language',
+                'pattern' => '%s.mo',
+                'text_domain' => null,
+            ],
+        ],
+    ],
     'shortcodes' => [
         'invokables' => [
             'noop' => Shortcode\Noop::class,
